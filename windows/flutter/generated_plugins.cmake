195632538e74bc3ee35bc10f8100c65c90cd4917
Vivek Ellappan <vivekebiit@gmail.com>
2025-04-15 23:51:12 +0530

    dart ignore

--- windows/flutter/generated_plugins.cmake
+++ windows/flutter/generated_plugins.cmake
@@ -3,6 +3,11 @@
 #
 
 list(APPEND FLUTTER_PLUGIN_LIST
+  file_selector_windows
+  permission_handler_windows
+  printing
+  share_plus
+  url_launcher_windows
 )
 
 list(APPEND FLUTTER_FFI_PLUGIN_LIST
